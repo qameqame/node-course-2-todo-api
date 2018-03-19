abc9abc67a6e06c072b1444871d8e6ba9662ae45
kameyama <hkame@jp.ibm.com>
2018-03-20 08:44:34 +0900

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const {ObjectId} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -106,6 +107,15 @@ app.post('/users',(req,res)=>{
     res.status(400).send(e);
   })
 });
+
+
+
+
+app.get('/users/me',authenticate,(req,res)=>{
+  res.send(req.user);
+});
+
+
 
 app.listen(port,()=>{
   console.log(`Started on port ${port}`);
