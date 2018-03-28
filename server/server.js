457a926788efeecd76672d4af77abf5895c2aae7
kameyama <hkame@jp.ibm.com>
2018-03-28 14:43:50 +0900

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -109,6 +109,20 @@ app.post('/users',(req,res)=>{
   })
 });
 
+//POST /users/login{email,password}
+app.post('/users/login',(req,res)=>{
+  var body = _.pick(req.body,['email','password']);
+
+  User.findByCredentials(body.email,body.password).then((user)=>{
+    user.generateAuthToken().then((token)=>{
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e)=>{
+    res.status(400).send();
+  })
+
+});
+
 
 
 
